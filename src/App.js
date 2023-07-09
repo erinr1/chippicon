6a8c28dfae6c8ab9b511ed6b0f4b92d4d8d17215
erinr1 <erin.ramos87@gmail.com>
2023-07-09 14:27:24 -0400

    updated details img, cover page, and seo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import NavBar from "./NavBar";
 import chippi1 from "./img/chippi1.png";
 import chippi2 from "./img/chippi2.png";
-import austin from "./img/austin.png";
+import txchippi from "./img/txchippi.png";
+
 import Footer from "./Footer";
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
       <NavBar />
       <div className="">
         <img src={chippi1} alt="chippi crowd" className="background-left " />
-        <img src={austin} className="austin" alt="texas" />
+        <img src={txchippi} className="austin" alt="texas" />
         <img src={chippi2} alt="chippi crowd" className="background-right" />
       </div>
       <Footer />
--- src/Useful.js
+++ src/Useful.js
@@ -8,9 +8,6 @@ export default function UsefulDetails() {
   return (
     <div className="Useful">
       <NavBar />
-      <div className="date mt-5">
-        September 10, 2023 <br /> Austin, Texas
-      </div>
       <div className="details-div">
         <img src={details} alt="party details" className="details-image" />
       </div>
