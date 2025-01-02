6d7cd5d5672e4b4e54b3761fed56795e4cbae59d
erinr1 <erin.ramos87@gmail.com>
2025-01-01 21:59:02 -0500

    updated cover photo and navbar buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import NavBar from "./NavBar";
 import chippi1 from "./img/chippi1.png";
 import chippi2 from "./img/chippi2.png";
-import chippicon2023 from "./img/chippicon2023.jpeg";
+import chippicon2025 from "./img/chippicon2025.jpeg";
 
 import Footer from "./Footer";
 
@@ -12,12 +12,17 @@ function App() {
     <div className="App ">
       <NavBar />
       <div className="">
-        <img src={chippi1} alt="chippi crowd" className="background-left " />
-        <img src={chippicon2023} className="austin" alt="Chippicon 2023" />
-        <img src={chippi2} alt="chippi crowd" className="background-right" />
-      </div>
-      <div className="next-year">
-        Stay tuned for ChippiCon 2024 - Location TBD
+        <img
+          src={chippi1}
+          alt="chippi crowd"
+          className="d-none background-left "
+        />
+        <img src={chippicon2025} className="chippi-2025" alt="Chippicon 2025" />
+        <img
+          src={chippi2}
+          alt="chippi crowd"
+          className="d-none background-right"
+        />
       </div>
       <Footer />
     </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -25,21 +25,6 @@ export default function NavBar() {
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link " href="/#">
-                {" "}
-                <Link to="/useful">
-                  <span className="navbar-links">Useful Details</span>
-                </Link>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link " href="/#">
-                <Link to="/useless">
-                  <span className="navbar-links">Useless Details</span>
-                </Link>
-              </a>
-            </li>
-            <li class="nav-item">
               <a
                 class="nav-link navbar-links"
                 href="https://linktr.ee/chippinft"
@@ -55,6 +40,22 @@ export default function NavBar() {
                 The OpenSea
               </a>
             </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/#">
+                {" "}
+                <Link to="/useful">
+                  <span className="navbar-links d-none">Useful Details</span>
+                </Link>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="/#">
+                <Link to="/useless">
+                  <span className="navbar-links d-none">Useless Details</span>
+                </Link>
+              </a>
+            </li>
+
             <li class="d-none nav-item">
               <a class="nav-link " href="/#">
                 <Link to="/secret">
