b7acd3685532edebe34697db131c039a151116c5
erinr1 <erin.ramos87@gmail.com>
2025-01-01 22:24:56 -0500

    links open to new tab

--- src/NavBar.js
+++ src/NavBar.js
@@ -28,6 +28,7 @@ export default function NavBar() {
               <a
                 class="nav-link navbar-links"
                 href="https://linktr.ee/chippinft"
+                target="_blank"
               >
                 Chippi LinkTree
               </a>
@@ -36,6 +37,7 @@ export default function NavBar() {
               <a
                 class="nav-link navbar-links"
                 href="https://opensea.io/collection/chippi"
+                target="_blank"
               >
                 The OpenSea
               </a>
@@ -44,6 +46,7 @@ export default function NavBar() {
               <a
                 class="nav-link navbar-links"
                 href="https://www.amazon.com/dp/B0DRZHHM3X/ref=sr_1_1?crid=JGJJ8AGYQUL6&dib=eyJ2IjoiMSJ9.u4Q1DIFW_Ezww2MSsYlHlVnBemoS3Nmvm7jeovBClR7eLrXk4sw8Wh-z2EKtzXa8jNF1FXAS-adgp1aZuCyzlSI_C_jj7MjE3C1xZp1d1NY.WUvuxMrC77B9pLWPrDvBThlGxpJaH_Xqz6bAJvwVPzY&dib_tag=se&keywords=what+is+chippi&qid=1735788078&sprefix=what+is+chippi%2Caps%2C122&sr=8-1"
+                target="_blank"
               >
                 What Is Chippi? Book
               </a>
